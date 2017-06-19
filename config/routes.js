e6c5f605533041f3c98f5e0e2c2f32dbf1a260bc
Douglas Bollella <Douglas Bollella>
2017-06-19 20:16:37 -0300

    Implementação da Rota de login

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = function(server){
+    
+    //Open Routes
+    const openApi = express.Router();
+    server.use('oapi',openApi)
+    
+    const AuthService = require('../api/user/authService');
+    openApi.post('/login', AuthService.login);
+    openApi.post('/singup', AuthService.singup);
+    openApi.post('/validateToken', AuthService.validateToken);
+    
     //API Routes
     const router = express.Router();
     server.use('/api', router);
@@ -8,4 +19,7 @@ module.exports = function(server){
    //rotas da API
    const billingCycleService = require('../api/billingCycle/billingCycleService');
    billingCycleService.register(router, '/billingCycles');
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
